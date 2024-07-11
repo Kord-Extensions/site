eae1aefcb4125f02e9e128a492ae438bfe5bc644
Gareth Coles <gdude2002@gmail.com>
2024-07-11 19:31:40 +0100

    Fix Groovy example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,12 +27,12 @@
 
 	maven {
 		name "Sonatype Snapshots (Legacy)"
-		url uri("https://oss.sonatype.org/content/repositories/snapshots")
+		url "https://oss.sonatype.org/content/repositories/snapshots"
 	}
 
 	maven {
 		name "Sonatype Snapshots"
-		url uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+		url "https://s01.oss.sonatype.org/content/repositories/snapshots"
 	}
 }
 
